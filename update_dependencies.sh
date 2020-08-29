ff6dbdf176f1b93a343e9101b36bdaf0492704af
furudbat <hircreacc@gmail.com>
2020-08-30 00:08:31 +0200

    clean up

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -9,10 +9,10 @@ npm i @fortawesome/fontawesome-free
 npm i mustache handlebars
 npm i clipboard
 npm i list.js
-npm i js-beautify json-parse-better-errors htmlhint
-npm i esprima js-yaml
+npm i js-beautify json-parse-better-errors
+npm i js-yaml
 npm i brace
-npm i localforage
+npm i localforage memory-cache
 
 npm i --save-dev typescript
 npm i --save-dev browserify tsify
@@ -20,21 +20,19 @@ npm i --save-dev unassertify envify uglifyify common-shakeify browser-pack-flat 
 npm i --save-dev @tsconfig/recommended
 npm i --save-dev @types/node
 npm i --save-dev @types/jquery
+npm i --save-dev @types/jqueryui
 npm i --save-dev @types/bootstrap
 npm i --save-dev @types/bootstrap-toggle
 npm i --save-dev @types/mustache
+npm i --save-dev @types/handlebars
 npm i --save-dev @types/list.js
-npm i --save-dev @types/htmlhint
 npm i --save-dev @types/json-parse-better-errors
 npm i --save-dev @types/js-beautify
 npm i --save-dev @types/js-yaml
-npm i --save-dev @types/brace
+npm i --save-dev @types/ace
 npm i --save-dev @types/clipboard
-npm i --save-dev @types/jqueryui
-
-
-
-
+npm i --save-dev @types/localforage
+npm i --save-dev @types/memory-cache
 
 cp node_modules/jquery/dist/jquery.min.* assets/js
 
@@ -52,37 +50,3 @@ mkdir assets/css/webfonts
 cp node_modules/@fortawesome/fontawesome-free/js/*.min.js assets/js/font-awesome
 cp node_modules/@fortawesome/fontawesome-free/css/*.min.css assets/css/font-awesome
 cp node_modules/@fortawesome/fontawesome-free/webfonts/*.* assets/css/webfonts
-
-mkdir assets/js/handlebars
-cp node_modules/handlebars/dist/*.min.* assets/js/handlebars
-mkdir assets/js/mustache
-cp node_modules/mustache/mustache.min.* assets/js/mustache
-
-mkdir assets/js/beautify
-cp node_modules/js-beautify/js/lib/beautify*.js assets/js/beautify
-
-mkdir assets/js/yaml
-cp node_modules/js-yaml/dist/*.min.js assets/js/yaml
-
-mkdir assets/js/esprima
-cp node_modules/esprima/dist/*.js assets/js/esprima
-
-mkdir assets/js/clipboard
-cp node_modules/clipboard/dist/*.min.js assets/js/clipboard
-
-mkdir assets/js/htmlhint
-cp node_modules/htmlhint/dist/*.min.js assets/js/htmlhint
-
-mkdir assets/js/csslint
-cp node_modules/csslint/dist/*.js assets/js/csslint
-
-mkdir assets/js/jsonlint
-cp node_modules/jsonlint/web/*.js assets/js/jsonlint
-
-mkdir assets/js/ace
-mkdir assets/js/ace/snippets
-cp node_modules/ace-builds/src-min-noconflict/*.js assets/js/ace
-cp node_modules/ace-builds/src-min-noconflict/snippets/*.js assets/js/ace/snippets
-
-mkdir assets/js/listjs
-cp node_modules/list.js/dist/*.min.js assets/js/listjs
