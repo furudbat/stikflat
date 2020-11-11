4ab99a30d3d61e484987944404c1e580272b705d
furudbat <hircreacc@gmail.com>
2020-11-11 23:28:38 +0100

    clean up

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -8,41 +8,6 @@
 
 rm -rf node_modules package-lock.json
 npm install
-npm i bootstrap@4 jquery@3 jquery-ui
-npm i bootstrap-toggle
-npm i @fortawesome/fontawesome-free
-npm i mustache handlebars hogan.js pug underscore dot
-npm i clipboard
-npm i list.js
-npm i js-beautify json-parse-better-errors
-npm i js-yaml
-npm i brace
-npm i localforage memory-cache
-
-npm i --save-dev typescript
-npm i --save-dev browserify tsify
-npm i --save-dev unassertify envify uglifyify common-shakeify browser-pack-flat uglify-es
-npm i --save-dev @tsconfig/recommended
-npm i --save-dev @types/node
-npm i --save-dev @types/jquery
-npm i --save-dev @types/jqueryui
-npm i --save-dev @types/bootstrap
-npm i --save-dev @types/bootstrap-toggle
-npm i --save-dev @types/mustache
-npm i --save-dev @types/handlebars
-npm i --save-dev @types/hogan.js
-npm i --save-dev @types/pug
-npm i --save-dev @types/underscore
-npm i --save-dev @types/dot
-npm i --save-dev @types/list.js
-npm i --save-dev @types/json-parse-better-errors
-npm i --save-dev @types/js-beautify
-npm i --save-dev @types/js-yaml
-npm i --save-dev @types/ace
-npm i --save-dev @types/clipboard
-npm i --save-dev @types/localforage
-npm i --save-dev @types/memory-cache
-
 
 cp node_modules/jquery/dist/jquery.min.* assets/js
 
